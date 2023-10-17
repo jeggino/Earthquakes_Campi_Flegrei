4ea2333864c8e795222b6a68b010e1b6a7437359
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-10-17 13:44:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,18 +7,18 @@ import pydeck as pdk
 
 
 # @st.cache_data(experimental_allow_widgets=True)  # 👈 Set the parameter
-# def get_data():
-inputs_months =st.number_input("Number of months", min_value=1, max_value=3, value="min", step=1, label_visibility="visible")
-today = datetime.now()
-last_month_date = today + relativedelta(months=-inputs_months)
+def get_data():
+  inputs_months =st.number_input("Number of months", min_value=1, max_value=3, value="min", step=1, label_visibility="visible")
+  today = datetime.now()
+  last_month_date = today + relativedelta(months=-inputs_months)
+  
+  df_raw = pd.read_csv(f"https://webservices.ingv.it/fdsnws/event/1/query?starttime={str(last_month_date.strftime('%Y-%m-%d'))}T00%3A00%3A00&endtime={str(today.strftime('%Y-%m-%d'))}T23%3A59%3A59&minmag=-1&maxmag=10&mindepth=-10&maxdepth=1000&minlat=35&maxlat=49&minlon=5&maxlon=20&minversion=100&orderby=time-asc&format=text&limit=10000",sep="|")
+  df_fun = df_raw[df_raw.EventLocationName=="Campi Flegrei"][['Time', 'Latitude', 'Longitude', 'Depth/Km','Magnitude','EventLocationName']]
+  
+  gdf = gpd.GeoDataFrame(df_fun, geometry=gpd.points_from_xy(df_fun.Longitude, df_fun.Latitude), crs="EPSG:4326")    
+  return gdf
 
-df_raw = pd.read_csv(f"https://webservices.ingv.it/fdsnws/event/1/query?starttime={str(last_month_date.strftime('%Y-%m-%d'))}T00%3A00%3A00&endtime={str(today.strftime('%Y-%m-%d'))}T23%3A59%3A59&minmag=-1&maxmag=10&mindepth=-10&maxdepth=1000&minlat=35&maxlat=49&minlon=5&maxlon=20&minversion=100&orderby=time-asc&format=text&limit=10000",sep="|")
-df_fun = df_raw[df_raw.EventLocationName=="Campi Flegrei"][['Time', 'Latitude', 'Longitude', 'Depth/Km','Magnitude','EventLocationName']]
-
-gdf = gpd.GeoDataFrame(df_fun, geometry=gpd.points_from_xy(df_fun.Longitude, df_fun.Latitude), crs="EPSG:4326")    
-  # return gdf
-
-# df = get_data()
+df = get_data()
 
 # Define a layer to display on a map
 layer = pdk.Layer(
