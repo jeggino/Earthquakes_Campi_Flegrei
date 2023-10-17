3005f086fc21b2d597b52afd60f09c36ca61ac42
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-10-17 18:57:29 +0200

    Update app.py

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ def get_data():
   return gdf
 
 body = "https://travelnostop.com/wp-content/uploads/2014/02/muqoy_campiflegrei-610x366.jpg"
-st.markdown(body, unsafe_allow_html=False, *, help=None)
+st.markdown(body, unsafe_allow_html=False, help=None)
 
 df = get_data()
 
