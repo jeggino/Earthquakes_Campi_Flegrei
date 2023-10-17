00be45032c198a364b5dcf41924b274d9edba80e
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-10-17 13:20:59 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,38 @@
+import pandas as pd
+from datetime import datetime 
+from dateutil.relativedelta import relativedelta 
+import geopandas as gpd
+import pydeck as pdk
+
+
+@st.cache_data(experimental_allow_widgets=True)  # 👈 Set the parameter
+def get_data():
+  inputs_months =st.number_input("Number of months", min_value=1, max_value=3, value="min", step=1, label_visibility="visible")
+  today = date.today()
+  last_month_date = now + relativedelta(months=-inputs_months)
+  
+  df_raw = pd.read_csv(f"https://webservices.ingv.it/fdsnws/event/1/query?starttime={str(last_month_date)}T00%3A00%3A00&endtime={str(today)}T23%3A59%3A59&minmag=-1&maxmag=10&mindepth=-10&maxdepth=1000&minlat=35&maxlat=49&minlon=5&maxlon=20&minversion=100&orderby=time-asc&format=text&limit=10000",sep="|")
+  df_fun = df_raw[df_raw.EventLocationName=="Campi Flegrei"][['Time', 'Latitude', 'Longitude', 'Depth/Km','Magnitude','EventLocationName']]
+  
+  gdf = gpd.GeoDataFrame(df_fun, geometry=gpd.points_from_xy(df_fun.Longitude, df_fun.Latitude), crs="EPSG:4326")    
+  return gdf
+
+df = get_data()
+
+# Define a layer to display on a map
+layer = pdk.Layer(
+    "ScreenGridLayer",
+    df,
+    pickable=True,
+    opacity=0.3,
+    cell_size_pixels=30,
+    get_position=["Longitude","Latitude"],
+)
+
+# Set the viewport location
+view_state = pdk.ViewState(latitude=df.Latitude.mean(), longitude=df.Longitude.mean(), zoom=11, bearing=0, pitch=0)
+
+# Render
+r = pdk.Deck(layers=[layer], initial_view_state=view_state, tooltip={"text": "Number of earthquakes: {cellCount}"})
+
+st.pydeck_chart(pydeck_obj=r, use_container_width=True)
